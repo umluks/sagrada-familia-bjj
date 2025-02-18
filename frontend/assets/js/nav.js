86ac5a93e0444b272d5e450041295e41cce66667
Lucas dos Anjos <lucas.sga@gmail.com>
2025-02-18 12:30:03 -0300

    backend

--- frontend/assets/js/nav.js
+++
@@ -1,6 +0,0 @@
-fetch("nav.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("menu").innerHTML = data;
-  })
-  .catch((error) => console.error("Erro ao carregar o menu:", error));
